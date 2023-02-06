723154b602a4154193b49dcaa1d92c9b128e2d60
kmagdic <karlo.magdic@gmail.com>
2023-02-06 14:00:16 +0100

    Osnovna verzija sa dodavanjem todoa i listanjem

---
+++ src/main/java/com/todoapp/todoapp/Todo.java
@@ -0,0 +1,33 @@
+package com.todoapp.todoapp;
+
+import java.util.Date;
+
+public class Todo {
+    private String title;
+    private Date date;
+
+    public Todo(String title) {
+        this.title = title;
+    }
+
+    public Todo(String title, Date date) {
+        this.title = title;
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
---
+++ src/main/java/com/todoapp/todoapp/TodoController.java
@@ -0,0 +1,54 @@
+package com.todoapp.todoapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Controller
+public class TodoController {
+
+    List<Todo> todoList = new ArrayList<>();
+
+    public TodoController() {
+        todoList.add(new Todo("Naučiti HTML", new Date()));
+        todoList.add(new Todo("Naučiti CSS", new Date()));
+        todoList.add(new Todo("Naučiti JS", new Date()));
+
+    }
+
+
+    @GetMapping("/todos")
+    public String showTodos(Model model) {
+        model.addAttribute(todoList);
+        return "employee_todo_list_user.html";
+    }
+
+
+    @GetMapping("/addNewTodo")
+    public String addNewTodo(String title) {
+        todoList.add(new Todo(title));
+
+        return "redirect:/todos";
+
+    }
+
+    @GetMapping("/delete")
+    public String delete(String title) {
+        for(Todo todo : todoList) {
+            if(todo.getTitle().equals(title)) {
+                todoList.remove(todo);
+                break;
+            }
+        }
+
+        return "redirect:/todos";
+
+    }
+
+
+}
